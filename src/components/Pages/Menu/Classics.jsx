ac2363853eb67a2c37fd11f633fcf1af4be678ef
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-09-14 18:20:32 -0400

    Variable sizing added.

--- src/components/Pages/Menu/Classics.jsx
+++ src/components/Pages/Menu/Classics.jsx
@@ -8,9 +8,24 @@ function Classics({ addToCart }) {
     const [alertMessage, setAlertMessage] = useState('');
     const [showModal, setShowModal] = useState(false);
     const [selectedPizza, setSelectedPizza] = useState(null);
+    const [selectedSize, setSelectedSize] = useState('small'); // Initialize with 'small'
 
-    const handleAddToCart = (item) => {
-        addItemToCart(addToCart, setAlertMessage, item); // Pass the item directly to the utility function
+    const basePrice = {
+        small: 16.75,
+        medium: 20.00,
+        large: 25.00,
+        'extra-large': 31.50,
+    };
+
+    const calculatePrice = (name, size) => {
+        // Calculate the price based on the pizza's name and size
+        return basePrice[size];
+    };
+
+    const handleAddToCart = (item, size) => {
+        const pizzaNameWithSize = `${size.charAt(0).toUpperCase() + size.slice(1)} ${item.name}`;
+        const price = calculatePrice(pizzaNameWithSize, size); // Calculate the price
+        addItemToCart(addToCart, setAlertMessage, { ...item, name: pizzaNameWithSize, price }); // Pass the item with modified name and calculated price
         closeModal();
     };
 
@@ -19,43 +34,38 @@ function Classics({ addToCart }) {
             id: 0,
             name: 'Deluxe',
             cdescrip: 'Pepperoni, Bacon, Mushrooms, Green Peppers & Onions',
-            price: 12.99,
         },
         {
             id: 1,
             name: 'Truly Canadian',
             cdescrip: 'Mozzarella and Cheddar Cheese, Pepperoni, Bacon & Mushrooms',
-            price: 11.99,
         },
         {
             id: 2,
             name: 'Big Meat',
             cdescrip: 'Pepperoni, Bacon, Sausage, Ham & Ground Beef',
-            price: 13.99,
         },
         {
             id: 3,
             name: 'Vegetarian',
             cdescrip: 'Mushrooms, Green Peppers, Onions, Tomatoes & Green Olives',
-            price: 10.99,
         },
         {
             id: 4,
             name: 'Hawaiian',
             cdescrip: 'Ham, Pineapple & Extra Cheese',
-            price: 11.99,
         },
         {
             id: 5,
             name: 'Chicken Bacon Ranch',
             cdescrip: 'Ranch Base, Cheeseblend, Grilled Chicken Strips, Bacon & Parm to Finish',
-            price: 14.99,
         },
     ];
 
     const openModal = (pizza) => {
         setSelectedPizza(pizza);
         setShowModal(true);
+        setSelectedSize('small'); // Reset selected size when opening the modal
     };
 
     const closeModal = () => {
@@ -84,37 +94,26 @@ function Classics({ addToCart }) {
                     <h2>{selectedPizza.name}</h2>
                     <p>{selectedPizza.cdescrip}</p>
                     <form className='size-form'>
-                        <div className='size-option'>
-                            <label>
-                                <p>Small</p>
-                                <input type="radio" name="pizzaSize" value="small" />
-                            </label>
-                        </div>
-                        <div className='size-option'>
-                            <label>
-                                <p>Medium</p>
-                                <input type="radio" name="pizzaSize" value="medium" />
-                            </label>
-                        </div>
-                        <div className='size-option'>
-                            <label>
-                                <p>Large</p>
-                                <input type="radio" name="pizzaSize" value="large" />
-                            </label>
-                        </div>
-                        <div className='size-option'>
-                            <label>
-                                <p>Extra Large</p>
-                                <input type="radio" name="pizzaSize" value="extra-large" />
-                            </label>
-                        </div>
+                        {Object.keys(basePrice).map((size) => (
+                            <div className='size-option' key={size}>
+                                <label>
+                                    <p>{size.charAt(0).toUpperCase() + size.slice(1)}</p>
+                                    <input
+                                        type="radio"
+                                        name="pizzaSize"
+                                        value={size}
+                                        checked={selectedSize === size}
+                                        onChange={() => setSelectedSize(size)}
+                                    />
+                                </label>
+                            </div>
+                        ))}
                     </form>
-                    <p>Price: ${selectedPizza.price.toFixed(2)}</p>
-                    <button onClick={() => handleAddToCart(selectedPizza)}>Add to Cart</button>
+                    <p>Price: ${calculatePrice(selectedPizza.name, selectedSize).toFixed(2)}</p>
+                    <button onClick={() => handleAddToCart(selectedPizza, selectedSize)}>Add to Cart</button>
                     <button onClick={closeModal}>Close</button>
                 </div>
             )}
-
         </Layout>
     );
 }
