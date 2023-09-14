b4f37405945312cf5e1d58e096f13735e0d5f0ee
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-09-14 19:13:18 -0400

    check

--- src/components/Pages/BYOPizza/BYOPizza.jsx
+++ src/components/Pages/BYOPizza/BYOPizza.jsx
@@ -1,20 +1,155 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../LayoutComp/Layout';
 
 function BYOPizza() {
+  const [selectedSize, setSelectedSize] = useState('');
+  const [selectedMeats, setSelectedMeats] = useState([]);
+  const [selectedCheeses, setSelectedCheeses] = useState([]);
+  const [selectedFruitVegetables, setSelectedFruitVegetables] = useState([]);
+
+  const handleSizeChange = (e) => {
+    setSelectedSize(e.target.value);
+  };
+
+  const handleMeatChange = (e) => {
+    const meat = e.target.value;
+    setSelectedMeats((prevMeats) =>
+      prevMeats.includes(meat)
+        ? prevMeats.filter((item) => item !== meat) // Deselect if already selected
+        : [...prevMeats, meat] // Select if not selected
+    );
+  };
+
+  const handleCheeseChange = (e) => {
+    const cheese = e.target.value;
+    setSelectedCheeses((prevCheeses) =>
+      prevCheeses.includes(cheese)
+        ? prevCheeses.filter((item) => item !== cheese)
+        : [...prevCheeses, cheese]
+    );
+  };
+
+  const handleFruitVegetableChange = (e) => {
+    const fruitVegetable = e.target.value;
+    setSelectedFruitVegetables((prevFruitVegetables) =>
+      prevFruitVegetables.includes(fruitVegetable)
+        ? prevFruitVegetables.filter((item) => item !== fruitVegetable)
+        : [...prevFruitVegetables, fruitVegetable]
+    );
+  };
+
   return (
     <Layout>
       <div className='header'>
         <h1>Build Your Pizza</h1>
       </div>
       <form className='BYOP-Creator'>
-        <h3>SELECT YOUR SIZE</h3>
-        <div className='BYOP-Creator-Sizes'></div>
-        <input type="radio" id="html" name="fav_language" value="HTML" />
-        <label for="html">HTML</label><br />
-        <input type="radio" id="css" name="fav_language" value="CSS" />
-        <label for="css">CSS</label><br />
-        <input type="radio" id="javascript" name="fav_language" value="JavaScript"></input>
+        <div className='BYOPContainer'>
+          <div className='Selection'>
+            <h3>SELECT YOUR SIZE</h3>
+            <div className='options'>
+              {['Small', 'Medium', 'Large', 'Extra-Large'].map((size) => (
+                <label key={size}>
+                  <input
+                    type='radio'
+                    name='size'
+                    value={size}
+                    checked={selectedSize === size}
+                    onChange={handleSizeChange}
+                  />
+                  {size}
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className='Selection'>
+            <h3>SELECT YOUR MEATS</h3>
+            <div className='options'>
+              {[
+                'Anchovies',
+                'Pepperoni',
+                'Chicken',
+                'Bacon',
+                'Italian Sausage',
+                'Ham',
+                'Ground Beef',
+              ].map((meat) => (
+                <label key={meat}>
+                  <input
+                    type='checkbox'
+                    name='meat'
+                    value={meat}
+                    checked={selectedMeats.includes(meat)}
+                    onChange={handleMeatChange}
+                  />
+                  {meat}
+                </label>
+              ))}
+              <p id='note'>Please note chicken will count as two toppings.</p>
+            </div>
+          </div>
+          <div className='Selection'>
+            <h3>SELECT YOUR CHEESES</h3>
+            <div className='options'>
+              {[
+                'Asiago Cheese',
+                'Extra Cheese',
+                'Cheddar Cheese',
+                'Feta Cheese',
+                'Parmesan Cheese',
+              ].map((cheese) => (
+                <label key={cheese}>
+                  <input
+                    type='checkbox'
+                    name='cheese'
+                    value={cheese}
+                    checked={selectedCheeses.includes(cheese)}
+                    onChange={handleCheeseChange}
+                  />
+                  {cheese}
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className='Selection'>
+            <h3>SELECT YOUR FRUITS/VEGETABLES</h3>
+            <div className='options'>
+              {[
+                'Pineapple',
+                'Onions',
+                'Green Olives',
+                'Italian Spices',
+                'Garlic',
+                'Garlic Butter Crust',
+                'Mushrooms',
+                'Red Onions',
+                'Black Olives',
+                'Hot Peppers',
+                'Green Peppers',
+                'Broccoli',
+                'Tomatoes',
+                'Roasted Red Peppers',
+                'Jalapeno Peppers',
+                'Extra Sauce (free)',
+              ].map((fruitVegetable) => (
+                <label key={fruitVegetable}>
+                  <input
+                    type='checkbox'
+                    name='fruitVegetable'
+                    value={fruitVegetable}
+                    checked={selectedFruitVegetables.includes(fruitVegetable)}
+                    onChange={handleFruitVegetableChange}
+                  />
+                  {fruitVegetable}
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className='SelectionF'>
+            <p><strong>Total is $X</strong></p>
+            <button id='checkB'>Add to Cart</button>
+          </div>
+        </div>
       </form>
     </Layout>
   );
