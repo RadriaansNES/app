e720e470e60c3840f45d2245e8ce6ba12d18b86f
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-09-15 14:13:01 -0400

    Combos succesfully implemented.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,12 +1,20 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 
+const generateUniqueID = () => {
+  return Math.floor(Math.random() * 10000); // Generate a random 4-digit number
+};
+
 export const addToCart = (item) => ({
   type: ADD_TO_CART,
-  payload: item,
+  payload: {
+    ...item,
+    id: generateUniqueID(),
+  },
 });
 
 export const removeFromCart = (itemId) => ({
   type: REMOVE_FROM_CART,
   payload: itemId,
 });
+
