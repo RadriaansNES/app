2f44e045da36509243a16cfd9e0e2818cca33577
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-09-18 14:56:34 -0400

    Hashrouter...?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import WhatsNew from './components/Pages/Menu/WhatsNew';
 import Menu from './components/Pages/Menu/Menu';
 import SideOrders from './components/Pages/Menu/SideOrders';
