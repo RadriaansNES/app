9bc9cbf1ebd41f074dc1ff83b97e4774d2e8ce74
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-09-18 18:04:59 -0400

    Queries addition.

--- src/components/LayoutComp/Navbar.jsx
+++ src/components/LayoutComp/Navbar.jsx
@@ -9,33 +9,49 @@ import { Link } from 'react-router-dom';
 
 function MenuNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Link to="/checkout">
-          <img src={cart} className='cart' alt='Cart for your order' />
-        </Link>
-        <Link to='/WhatsNew'>
-          <img src={logo} alt='Logo' id='headerLogo' />
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
-      </Container>
-      <Container id='dropdown'>
+    <div className='TopMenu'>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Link to="/checkout">
+            <img src={cart} className='cart' alt='Cart for your order' />
+          </Link>
+          <Link to='/WhatsNew'>
+            <img src={logo} alt='Logo' id='headerLogo' />
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
+        </Container>
+        <Container id='dropdown'>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/WhatsNew" element={WhatsNew}>DEALS</Nav.Link>
+              <Nav.Link href="/Menu" element={Menu}>MENU</Nav.Link>
+              <Nav.Link href="/Subs">SUBS</Nav.Link>
+              <Nav.Link href="/Salads">SALADS</Nav.Link>
+              <Nav.Link href="/Sides">SIDE ORDERS</Nav.Link>
+              <Nav.Link href="/Beverages">BEVERAGES</Nav.Link>
+              {/* <Nav.Link href="/DeliveryMap">DELIVERY MAP</Nav.Link> */}
+              <Nav.Link href="tel:7055666969">CALL 1955 LASALLE BLVD.</Nav.Link>
+              <Nav.Link href="tel:7055222828">CALL 1769 REGENT ST. S.</Nav.Link>
+              <Nav.Link href="tel:7055885858">CALL 3020 HWY. 69 N.</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar >
+      <Container id='hide'>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto" id='NavF'>
             <Nav.Link href="/WhatsNew" element={WhatsNew}>DEALS</Nav.Link>
             <Nav.Link href="/Menu" element={Menu}>MENU</Nav.Link>
             <Nav.Link href="/Subs">SUBS</Nav.Link>
             <Nav.Link href="/Salads">SALADS</Nav.Link>
             <Nav.Link href="/Sides">SIDE ORDERS</Nav.Link>
             <Nav.Link href="/Beverages">BEVERAGES</Nav.Link>
-            <Nav.Link href="/DeliveryMap">DELIVERY MAP</Nav.Link>
-            <Nav.Link href="tel:7055666969">CALL 1955 LASALLE BLVD.</Nav.Link>
-            <Nav.Link href="tel:7055222828">CALL 1769 REGENT ST. S.</Nav.Link>
-            <Nav.Link href="tel:7055885858">CALL 3020 HWY. 69 N.</Nav.Link>
+            <Nav.Link href="/checkout">CART</Nav.Link>
+            {/* <Nav.Link href="/DeliveryMap">DELIVERY MAP</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar >
+    </div>
   );
 }
 
