8917a3a2901be391ad77432d48e9f41eb3767729
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-09-21 11:13:27 -0400

    Config removal.

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    # Define your database URI here
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:SQLpass6432@localhost:3306/mydb'
-    # Other configuration options
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
