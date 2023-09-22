696dad4b79262ccbdd45527ae5561b74d9ae42fa
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-09-22 10:47:31 -0400

    Revision for frontend to be served by backend.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 from flask_login import LoginManager
 from models.users import User, db
 from flask_cors import CORS 
-app = Flask(__name__)
-app.config.from_object(Config)
 
+app = Flask(__name__, static_folder='../app/build/static', template_folder='../app/build')
+
+app.config.from_object(Config)
 
 CORS(app) ##Enable CORSs
 
@@ -15,9 +16,13 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
+
 @login_manager.user_loader
 def load_user(user_id):
- 
     return User.query.get(int(user_id))
 
 if __name__ == '__main__':
